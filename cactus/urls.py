c2ff76563a4405291ecbda4f63cea6162823faa9
CostVictor <costvictor36@gmail.com>
2025-01-12 21:39:22 -0300

    feat(lunchSC): Acesso ao estoque de almoços através de uma conexão websocket

--- cactus/urls.py
+++ cactus/urls.py
@@ -2,6 +2,7 @@ import os
 from django.contrib import admin
 from django.urls import path, include, re_path
 from snackSC import consumers as snack_consumers
+from lunchSC import consumers as lunch_comsumers
 
 urlpatterns = [
     path(os.getenv("ADMIN_PANEL"), admin.site.urls),
@@ -11,4 +12,7 @@ urlpatterns = [
     path("lunch/", include("lunchSC.urls"), name="lunch"),
 ]
 
-websocket_urlpatterns = [re_path(r"^snack/$", snack_consumers.SnacksConsumer.as_asgi())]
+websocket_urlpatterns = [
+    re_path(r"^snack/$", snack_consumers.SnacksConsumer.as_asgi()),
+    re_path(r"^lunch/$", lunch_comsumers.LunchConsumer.as_asgi()),
+]
