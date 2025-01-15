df8500e1f92d7d303fbcfc386ad1ea85018c177f
CostVictor <costvictor36@gmail.com>
2025-01-15 03:24:29 -0300

    fix: rota de ingredientes não é encontrada

--- lunchSC/urls.py
+++ lunchSC/urls.py
@@ -9,16 +9,16 @@ from .views import (
 
 urlpatterns = [
     path("", LunchWeekView.as_view(), name="lunch_week"),
-    path("<str:dish_name>/", DishView.as_view(), name="dish"),
-    path(
-        "<str:dish_name>/<str:ingredient_name>/",
-        CompositionView.as_view(),
-        name="composition",
-    ),
     path("ingredients/", IngredientsView.as_view(), name="ingredients"),
     path(
         "ingredients/<str:ingredient_name>/",
         IngredientView.as_view(),
         name="ingredient",
+    ),
+    path("<str:dish_name>/", DishView.as_view(), name="dish"),
+    path(
+        "<str:dish_name>/<str:ingredient_name>/",
+        CompositionView.as_view(),
+        name="composition",
     ),
 ]
